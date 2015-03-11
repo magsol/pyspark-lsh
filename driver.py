78dc0979684d4a44ccd87352346bb1830bd63ed4
Shannon Quinn <magsol@gmail.com>
2015-03-11 14:52:47 -0400

    Provided a utility method for reading the text form of the dataset from the original spark-hash project.

--- driver.py
+++ driver.py
@@ -6,15 +6,21 @@ from pyspark.mllib.linalg import SparseVector
 
 import lsh
 
+def read_text(sc, path):
+    text = sc.textFile(path).collect()
+    data = []
+    for s in text:
+        elements = map(int, s[s.index("[") + 1:s.index("]")].split(","))
+        v = SparseVector(65535, elements, np.ones(len(elements)))
+        data.append(v)
+    return data
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = 'Spark LSH',
         epilog = 'lol lsh', add_help = 'How to use',
         prog = 'python driver.py <arguments>')
     parser.add_argument("-i", "--input", required = True,
         help = "Input directory of text files.")
-    parser.add_argument("-p", "--primer", type = int, default = None,
-        help = "An integer larger than the largest value in your data. This " +
-            "primes the hashing functions and must be specified.")
 
     # Optional parameters.
     parser.add_argument("-m", "--bins", type = int, default = 1000,
@@ -27,17 +33,16 @@ if __name__ == "__main__":
     parser.add_argument("-b", "--bands", type = int, default = 25,
         help = "Number of bands. Each band will have (n / b) elements. Larger " +
             "numbers of elements increase confidence in element similarity. [DEFAULT: 25]")
-    parser.add_argument("-c", "--minbucketsize", type = int, default = 0,
+    parser.add_argument("-c", "--minbucketsize", type = int, default = 2,
         help = "Minimum bucket size (0 to disable). Buckets with fewer than this " +
-            "number of elements will be dropped. [DEFAULT: 0]")
+            "number of elements will be dropped. [DEFAULT: 2]")
 
     args = vars(parser.parse_args())
     sc = SparkContext(conf = SparkConf())
 
-    # Read the input data and count the number of elements.
-    #data = sc.textFile(args['input']).zipWithIndex()
-    rawdata = np.random.binomial(1, 0.01, size = (275, 65535))
-    data = [SparseVector(65535, np.where(row > 0)[0], np.ones(row[row > 0].shape[0])) for row in rawdata]
-    zdata = sc.parallelize(data).zipWithIndex()
-    p, m, n, b, c = args['primer'], args['bins'], args['numrows'], args['bands'], args['minbucketsize']
-    print lsh.run(zdata, p, m, n, b, c)
+    # Read the input data.
+    data = read_text(sc, args['input'])
+    p = 65537
+    m, n, b, c = args['bins'], args['numrows'], args['bands'], args['minbucketsize']
+    model = lsh.run(data, p, m, n, b, c)
+    print 'Found %s clusters.' % model.buckets.count()
